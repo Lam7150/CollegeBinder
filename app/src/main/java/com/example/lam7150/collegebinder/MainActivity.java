cc10700e4891baf36ac0bf4920f15a067e7e7f1b
israelreyes2 <ir2@illinois.edu>
2018-12-03 15:26:25 -0600

    first commit

--- app/src/main/java/com/example/lam7150/collegebinder/MainActivity.java
+++ app/src/main/java/com/example/lam7150/collegebinder/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.lam7150.collegebinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /** Called when user touches the button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
