7aad27db00d9ba2f1659931607946f244794457b
israelreyes2 <ir2@illinois.edu>
2018-12-12 03:37:06 -0600

    most of the styling is done, search activity was remade to extract and carry on string value of the clicked school for use in the next activity.

--- app/src/main/java/com/example/lam7150/collegebinder/CSVFile.java
+++ app/src/main/java/com/example/lam7150/collegebinder/CSVFile.java
@@ -6,20 +6,20 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-//public class for reading csv file which contains all or colleges from the API.
-// website for reference:
-// https://stackoverflow.com/questions/19974708/reading-csv-file-in-resources-folder-android
+/**public class for reading csv file which contains all or colleges from the API.
+ * website for reference:
+ * https://stackoverflow.com/questions/19974708/reading-csv-file-in-resources-folder-android */
 
 public class CSVFile {
     InputStream inputStream;
 
-    //constructor for inputStream, InputStream allows us to read files
-    // , in this case it will be CSV
+    /**constructor for inputStream, InputStream allows us to read files
+     * in this case it will be CSV */
     public CSVFile(InputStream inputStream) {
         this.inputStream = inputStream;
     }
-    //reads line of CSV file, luckily we do not have to parse.
-    //We just read line by line and get a String value for that line and add to collegeList
+    /**reads line of CSV file, luckily we do not have to parse.
+     * We just read line by line and get a String value for that line and add to collegeList */
     public List read() {
         List returnList = new ArrayList();
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
--- app/src/main/java/com/example/lam7150/collegebinder/MainActivity.java
+++ app/src/main/java/com/example/lam7150/collegebinder/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
